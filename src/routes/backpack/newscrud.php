070305511a761f80a4ec28ab07987745f5f93158
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-07-06 07:40:37 +0300

    Update newscrud.php

--- src/routes/backpack/newscrud.php
+++ src/routes/backpack/newscrud.php
@@ -5,7 +5,7 @@
 | Backpack\NewsCRUD Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define or change all of the routes that are
+| This file is where you may define all of the routes that are
 | handled by the Backpack\NewsCRUD package.
 |
 */
