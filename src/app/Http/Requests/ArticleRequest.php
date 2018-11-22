c9742b5c6ce09d11f014db5a3b3681dc70b39583
rppudo <30377205+rppudo@users.noreply.github.com>
2018-11-22 13:13:10 +0700

    Update request authorize

--- src/app/Http/Requests/ArticleRequest.php
+++ src/app/Http/Requests/ArticleRequest.php
@@ -12,7 +12,7 @@ class ArticleRequest extends \Backpack\CRUD\app\Http\Requests\CrudRequest
     public function authorize()
     {
         // only allow updates if the user is logged in
-        return \Auth::check();
+        return backpack_auth()->check();
     }
 
     /**
