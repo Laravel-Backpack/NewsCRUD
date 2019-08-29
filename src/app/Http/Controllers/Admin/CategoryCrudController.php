864794b396a2cf2ce6dfb874bbd676429df624d1
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-08-29 15:40:58 +0300

    enabled Reorder operation for Categories

--- src/app/Http/Controllers/Admin/CategoryCrudController.php
+++ src/app/Http/Controllers/Admin/CategoryCrudController.php
@@ -13,6 +13,7 @@ class CategoryCrudController extends CrudController
     use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ReorderOperation;
     
 
     public function setup()
