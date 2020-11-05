934f98b15bb48e13dc9aa98682e70fa0d8d4516a
Rodrigo Alvarez <44035269+GrnPero@users.noreply.github.com>
2020-11-05 11:20:51 -0800

    Code was not align

--- src/database/migrations/2015_08_04_130520_create_articles_table.php
+++ src/database/migrations/2015_08_04_130520_create_articles_table.php
@@ -15,8 +15,8 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('category_id')->unsigned();
-	    $table->string('title');
-	    $table->string('author');
+	        $table->string('title');
+	        $table->string('author');
             $table->string('social_media')->nullable();
             $table->string('slug')->default('');
             $table->text('content');
